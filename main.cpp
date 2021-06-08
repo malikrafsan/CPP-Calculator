c9aa85f87439eab40851be68c1deabfcb5386ba3
Malik Akbar Hashemi Rafsanjani <pro.malikakbar2357@gmail.com>
2021-06-08 12:54:18 +0700

    add comments in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,40 +2,58 @@
 #include <vector>
 using namespace std;
 
-
 double pangkat(double num1, double num2){
-    if(num2==0)
+	// I.S. num1 dan num2 terdefinisi
+	// F.S. dikembalikan nilai num1 pangkat num2 dengan metode rekursif
+    
+	// Base case
+	if(num2==0)
         return 1;
+	// Recursive case
     else
         return num1 * pangkat(num1,(num2-1));
 }
 
-
 double f(double x, vector<double> &lst) {
-  double hasil = 0;
-  for(int i{0}; i<lst.size(); ++i) hasil += (pangkat(x,i)) * lst[i];
-  return hasil;
+	// I.S. x dan lst terdefinisi
+	// F.S. dikembalikan nilai f(x)
+	
+	// KAMUS
+	double hasil = 0;
+	
+	// ALGORITMA
+	// Menghitung nilai f(x) untuk masing masing derajat f(x) dan menjumlahkannya 
+	for(int i{0}; i<lst.size(); ++i) hasil += (pangkat(x,i)) * lst[i];
+	return hasil;
 }
 
 double integral(double delta, double bawah, double atas, vector<double> &lst) {
-  double sum = 0;
-  while(bawah < atas) {
-    double luas = 0.5 * delta * (f(bawah, lst) + f((bawah + delta), lst));
-    sum += luas;
-    bawah += delta;
-  }
-  return sum;
+	// I.S. delta, bawah, atas, dan lst terdefinisi
+	// F.S. dikembalikan nilai integral dengan metode jumlah riemann tengah, dengan dx, yaitu delta
+	// batas bawah, yaitu bawah, batas atas yaitu atas, dan koefisien masing-masing derajat polinom pada lst
+
+	// KAMUS
+	double sum = 0;
+
+	// ALGORITMA
+	// Melakukan penjumlahan luas untuk setiap delta
+	while(bawah < atas) {
+		double luas = 0.5 * delta * (f(bawah, lst) + f((bawah + delta), lst));
+		sum += luas;
+		bawah += delta;
+	}
+	return sum;
 }
 
 
-int main()
-{
+int main() {
+	// KAMUS
     char op;
     float num1;
 	int num2;
 
-	cout << "Calculator C++"<<endl;
-	cout <<endl;
+	// Menampilkan main menu
+	cout << "Calculator C++\n"<<endl;
 	cout << "Kalkulator ini memiliki, berbagai fitur yaitu: "<<endl;
 	cout << "1. Operasi tambah (ketik '+')"<<endl;
 	cout << "2. Operasi kurang (ketik '-')"<<endl;
@@ -45,69 +63,58 @@ int main()
 	cout << "6. Operasi integral (ketik 'S')"<<endl;
 	cout << "-------------------------------------------------"<<endl;
 	
+	// Pengganti nilai boolean
 	char lanjut;
 	lanjut = 'Y';
 	
 	while (lanjut == 'Y'){
-		cout << "Masukkan operator"<<endl;
-	    cin >> op;
+		cout << "Masukkan operator: "<<endl; cin >> op;
 		
+		// Kasus operator sederhana
 		if (op == '+' || op == '-' || op == 'x' || op == '/'){
 		    cout << "Masukan dua bilangan (pisahkan dengan spasi): ";
 		    cin >> num1 >> num2;
 		
-		    switch(op)
-		    {
-		        case '+':
+		    switch(op) {
+		        case '+': // Operasi tambah
 		            cout <<"Hasil: "<< num1+num2<<endl;
 		            break;
 		
-		        case '-':
+		        case '-': // Operasi kurang
 		            cout <<"Hasil: "<< num1-num2<<endl;
 		            break;
 		
-		        case 'x':
+		        case 'x': // Operasi kali
 		            cout <<"Hasil: "<< num1*num2<<endl;
 		            break;
 		
-		        case '/':
-		            cout << "Hasil: "<< num1/num2<<endl;
+		        case '/': // Operasi bagi
+		            cout << "Hasil: "<< num1/num2<<endl; // num1 bertipe float sehingga akan menghasilkan tipe float juga
 		            break;
 		    }
-		} else if (op == 'S'){
-			cout <<"Kalkulator ini hanya bisa untuk fungsi polinom, berpangkat bulat dengan pangkat nol atau positif"<<endl;
+		// Operasi integral
+		} else if (op == 'S'){ 
+			cout <<"\n*Kalkulator ini hanya bisa untuk fungsi polinom, berpangkat bulat dengan pangkat nol atau positif"<<endl;
 			
 			cout <<"Masukkan pangkat tertinggi dari polinom anda"<<endl;
 			int n; cin >> n;
 			n = n + 1;
+			// Membuat array dinamis dengan vektor untuk menyimpan koefisien dari masing masing pangkat fungsi f(x)
 			vector<double> v(n);
 			
-			
+			// Mengisi array koefisien pangkat dari input user
 			for(int j{0};j < n; j++) {
 				cout << "Masukkan koefisien dari x pangkat "<<j<<endl;
 				cin >> v[j];	
-			} 
+			}
+			// Meminta input boundary integral dari user
 	  		double bawah, atas, delta;
-	  		cout << "Masukkan batas bawah"<<endl;
-	  		cin >> bawah;
-	  		cout << "Masukkan batas atas"<<endl;
-			cin >> atas; 
-			cout << "Masukkan delta x (besar langkah)"<<endl;
-			cin >> delta;
+	  		cout << "Masukkan batas bawah"<<endl; cin >> bawah;
+	  		cout << "Masukkan batas atas"<<endl; cin >> atas; 
+			cout << "Masukkan delta x (besar langkah)"<<endl; cin >> delta;
 	  		
+			// Menampilkan hasil ke layar
 			cout << "Hasilnya: "<< integral(delta, bawah, atas, v) << endl;
-			
-		} else if (op=='^') {
-			cout << "Kalkulator ini hanya menerima bilangan pangkat berupa bilangan bulat (positif atau negatif)"<<endl;
-			cout << "Masukan dua bilangan (pisahkan dengan spasi): ";
-			cin >> num1 >> num2;
-			
-			if (num2 >= 0) {
-				cout << "Hasil: " << pangkat(num1,num2)<<endl;
-			} else {
-				num2 = - num2;
-				
-				cout << "Hasil: " << 1 / pangkat(num1,num2)<<endl;
 
 			/* Contoh test case:
 			integral x^2 + 2x + 5 dari x = 3 sampai 5 dengan delta x sebesar 0.0001
@@ -116,16 +123,29 @@ int main()
 			
 			Kalkulator ini hanya bisa untuk fungsi polinom, berpangkat bulat dengan pangkat nol atau positif
 			*/
+
+		// Operasi pangkat			
+		} else if (op=='^') {
+			cout << "\nKalkulator ini hanya menerima bilangan pangkat berupa bilangan bulat (positif atau negatif)"<<endl;
+			cout << "Masukan dua bilangan (bil1 pangkat bil2): ";
+			cin >> num1 >> num2;
+			
+			// Kasus pangkat bilangan positif / nol
+			if (num2 >= 0) {
+				cout << "Hasil: " << pangkat(num1,num2)<<endl;
+			} 
+			// Kasus pangkat bilangan negatif
+			else {
+				num2 = - num2;
+				cout << "Hasil: " << 1 / pangkat(num1,num2)<<endl;
 			}
 		} else {
 		    cout << "Error! operator yang anda masukan tidak valid"<<endl;
 		}
-		cout << "Ada lagi? (Ketik 'Y' untuk lanjut dan apapun untuk tidak) "<<endl;
-		cin >> lanjut;
+		cout << "Ada lagi? (Ketik 'Y' untuk lanjut dan apapun untuk tidak) "<< endl; cin >> lanjut;
 	}
 	
 	cout << "Terima kasih. Semoga bermanfaat ^_^"<<endl;
-	
     return 0;
 }
 
